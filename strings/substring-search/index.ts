210f8ce2eeeed2a765893d38a7e8d0b1502539ab
boksimamba3 <boriskostres0@gmail.com>
2022-01-02 18:01:18 +0100

    Substring search

---
+++ strings/substring-search/index.ts
@@ -0,0 +1,16 @@
+function search(pattern: string, text: string) {
+  const n = text.length
+  const m = pattern.length
+
+  for (let i = 0; i <= n - m; i++) {
+    let j
+    for (j = 0; j < m; j++) {
+      if (text.charCodeAt(i + j) !== pattern.charCodeAt(j)) break
+    }
+    if (j === m) {
+      return i
+    }
+  }
+
+  return n
+}
